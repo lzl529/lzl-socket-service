85bd0b5ef1bf005a082d0a998fbc2cf106154eb9
wb-lzl441545 <wb-lzl441545@autonavi.com>
2020-04-21 15:40:22 +0800

    回退更改

--- app/io/controller/chat.js
+++ app/io/controller/chat.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const Controller = require('egg').Controller;
-const room = 'default_room';
 
 
 class ChatController extends Controller {
@@ -12,7 +11,7 @@ class ChatController extends Controller {
     const msg = ctx.args[0] || {};
     const { socket: { id } } = ctx;
     console.log(`接收到的聊天内容为：id: ${id} -->> ${msg}`);
-    nsp.to(room).emit('receive-msg', { text: `马甲-${id}：${msg}`, key: `${id}-${+new Date()}` });
+    nsp.emit('receive-msg', { text: `马甲-${id}：${msg}`, key: `${id}-${+new Date()}` });
   }
 }
 module.exports = ChatController;
